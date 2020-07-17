b1b93a560f5daa9777865822ac1012586879cf9d
shinleon <shinleon@uw.edu>
2020-07-16 19:19:33 -0700

    this is it. I'm gonna leave this alone

--- computer.h
+++ computer.h
@@ -3,4 +3,16 @@
 #include "environment.h"
 #include "wordnode.h"
 
-float evalCompNode(struct compNode* node, struct environmentNode* env, struct wordnode* dependencies, int* errorCode);
+//Evaluates the provided compNode "node"
+// returns a float that equals the value of the node 
+// dependencies is recursively called inside, and should initially
+// be passed in as NULL, unless there are variable names you want to ban
+// the int* errorCode should be passed in as a pointer to a zero, and
+// will return as zero if there were no errors in computing
+// otherwise it will be a value determined in computer.c's define statements
+// The parameter env lists the current environment. It it made of the variables
+// currently active in the program.
+float evalCompNode( struct compNode* node, 
+                    struct environmentNode* env, 
+                    struct wordnode* dependencies, 
+                    int* errorCode);
